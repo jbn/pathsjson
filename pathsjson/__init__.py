be34dc3036ead71d4c697cdc196476c902d0b2d9
John B Nelson <jbn@artifexdeus.com>
2017-10-25 12:03:53 -0700

    Update homepage for pypi

--- pathsjson/__init__.py
+++ pathsjson/__init__.py
@@ -1,6 +1,6 @@
 __title__ = "pathsjson"
 __description__ = ""
-__uri__ = "https://github.com/jbn/pathsjson"
+__uri__ = "https://pathsjson.falsifiable.com"
 __doc__ = __description__ + " <" + __uri__ + ">"
 __license__ = "MIT"
 __copyright__ = "Copyright (c) 2017 John Bjorn Nelson"
