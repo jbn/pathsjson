7eefe3fbc056ebb137d593ea3f199b2b4cb9129d
John B Nelson <jbn@artifexdeus.com>
2017-10-25 16:09:47 -0700

    Add --init to cli

--- pathsjson/cli.py
+++ pathsjson/cli.py
@@ -1,4 +1,6 @@
 import argparse
+import json
+import os
 import sys
 from pathsjson.impl import *
 
@@ -18,6 +20,10 @@ def main(args=None):
                         action='store_true',
                         help='Create the global paths.json file')
 
+    parser.add_argument('--init',
+                        action='store_true',
+                        help='Create a .paths.json file in the cwd')
+
     args = parser.parse_args(args)
 
     if args.print_global_path:
@@ -34,5 +40,14 @@ def main(args=None):
     if args.init_globals:
         print(create_user_globals_file())
         sys.exit(0)
+
+    if args.init:
+        if os.path.exists(".paths.json"):
+            raise OSError(".paths.json already exists!")
+
+        with open(".paths.json", "w") as fp:
+            json.dump({"__ENV": {}}, fp, indent="    ")
+            sys.exit()
+
 
     parser.print_help()
